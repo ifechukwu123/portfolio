47da19272fbb4f95fe29e56a122c6229f3adba18
Ife Onuorah <ifeonuorah@gmail.com>
2025-01-15 11:53:54 -0400

    Add font files, set global styling variables and update project structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				logo: ["var(--font-logo)", "serif"],
+				heading: ["var(--font-heading)", "serif"],
+				body: ["var(--font-body)", "sans-serif"],
+			},
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
