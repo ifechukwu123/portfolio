4f7d2daebdd33929c4e2f8b60bfa09415f9896d1
Ife Onuorah <ifeonuorah@gmail.com>
2025-01-20 23:52:05 -0400

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 
 export const metadata: Metadata = {
-	title: "Ife Onuorah",
+	title: "Ife Onuorah | Software Developer",
 	description: "Portfolio page for Ife Onuorah",
 };
 
