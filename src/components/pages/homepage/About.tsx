7b2c796a91343918fff3eb2524ad4c856f8be2a8
Ife Onuorah <ifeonuorah@gmail.com>
2025-06-05 16:54:29 -0300

    Update font sizes for headings in tablet screen & style about section on homepage for tablet screen

--- src/components/pages/homepage/About.tsx
+++ src/components/pages/homepage/About.tsx
@@ -9,24 +9,23 @@ export default function About() {
 	const router = useRouter();
 
 	return (
-		<section className="px-sm py-[2rem] md:px-md lg:px-lg h-[calc(100vh-4.4375rem)] min-h-[35.625rem] md:h-[calc(100vh-5.4375rem)] flex flex-col-reverse md:flex-row justify-center items-center gap-4 md:gap-4 lg:gap-24">
-			<div className="flex-1 flex flex-col gap-6 md:gap-9 md:flex-1 md:max-w-[31.25rem]">
-				<div className="flex flex-col items-center md:items-start">
+		<section className="px-sm py-[2rem] md:px-[6rem] lg:px-lg h-[calc(100vh-4.4375rem)] min-h-[35.625rem] md:h-[calc(100vh-4.9375rem)] flex flex-col-reverse md:flex-row justify-center md:justify-around items-center gap-4 md:gap-6 lg:gap-24">
+			<div className="flex-1 flex flex-col gap-6 md:gap-9 md:flex-1 md:max-w-[31.5625rem]">
+				<div className="flex flex-col items-center md:items-start md:gap-4">
 					<h1 className="capitalize flex items-baseline gap-1 md:gap-2">
 						<span>hello!</span>{" "}
 						<Image
 							src={SmileIcon}
 							alt="smiling face emoji"
-							className="w-[1.5625rem] md:w-[2.75rem] dark:invert"
+							className="w-[1.5625rem] md:w-[1.9375rem] dark:invert"
 						/>
 					</h1>
-					<p className="px-2 mt-[-0.5rem] max-w-[22.25rem] md:max-w-[31.25rem] text-center leading-[1.75rem] text-[1.125rem] md:text-2xl md:text-left md:leading-[2.5rem]">
+					<p className="px-2 md:px-0 max-w-[22.25rem] md:max-w-[31.5625rem] text-center text-[1.125rem] leading-[1.75rem] md:text-xl md:leading-[2.5rem] md:text-left">
 						I&#39;m Ife, a full-stack software developer with a simple yet
-						powerful approach: &ldquo;
-						<span className="italic capitalize text-rose-red">
+						powerful approach:{" "}
+						<span className="inline-block italic capitalize text-rose-red">
 							impact matters more than intent
-						</span>{" "}
-						&rdquo;
+						</span>
 					</p>
 				</div>
 
@@ -40,7 +39,7 @@ export default function About() {
 			<Image
 				src={profilePic}
 				alt="A picture of me, Ife!"
-				className="flex-1 h-12 w-auto md:flex-1 md:max-w-[20.625rem]"
+				className="flex-1 h-12 md:h-auto w-auto md:w-full md:max-w-[18.75rem]"
 			/>
 		</section>
 	);
