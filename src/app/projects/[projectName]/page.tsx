d60af28ea2e9e2b2d64feb5db50467675b9d37ec
Ife Onuorah <ifeonuorah@gmail.com>
2025-07-17 16:08:19 -0300

    set up basic layout for specific project page

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -2,6 +2,10 @@
 import { useParams, useRouter } from "next/navigation";
 import { ProjectsData } from "data/projectsData";
 import { useEffect } from "react";
+import launchIcon from "/public/icons/launch.svg?url";
+import githubIcon from "/public/icons/tools/github.svg?url";
+import IconButton from "@/components/ui/IconButton";
+import Image from "next/image";
 
 export default function Project() {
 	const router = useRouter();
@@ -19,5 +23,60 @@ export default function Project() {
 
 	if (!project) return null;
 
-	return <div className="px-sm">{project.name}</div>;
+	return (
+		<div className="px-sm pt-8 pb-24 md:px-md md:pt-16 md:pb-40 lg:px-lg">
+			<div className="flex gap-4 justify-between items-center flex-wrap">
+				<h1 className="flex gap-4">{project.name}</h1>
+				<div>
+					{project.githubLink && (
+						<IconButton
+							icon={githubIcon}
+							ariaLabel="Check out the github repository"
+							iconSize="regular"
+							onClick={() => {
+								window.open(project.githubLink, "_blank");
+							}}
+						/>
+					)}
+					{project.deployedLink && (
+						<IconButton
+							icon={launchIcon}
+							ariaLabel="Check out the live site"
+							iconSize="regular"
+							onClick={() => {
+								window.open(project.deployedLink, "_blank");
+							}}
+						/>
+					)}
+				</div>
+			</div>
+			<section>
+				<Image
+					src={project.image}
+					alt={`Screenshot of ${project.name} project`}
+					className="w-full"
+				/>
+			</section>
+			<section>
+				<h2>Overview</h2> <p>{project.overview}</p>
+			</section>
+			<section>
+				<h2>Key Features</h2>{" "}
+				<ol>
+					{project.features.map((feature) => (
+						<li key={feature.id}>
+							<h3 className="font-bold">{feature.name}:</h3>{" "}
+							<span>{feature.description}</span>
+						</li>
+					))}
+				</ol>
+			</section>
+			<section>
+				<h2>Tech Stack</h2>
+			</section>
+			<section>
+				<h2>What I learned</h2>
+			</section>
+		</div>
+	);
 }
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -9,6 +9,8 @@ interface Project {
 	githubLink?: string;
 	deployedLink?: string;
 	image: StaticImageData;
+	overview: string;
+	features: Array<{ id: number; name: string; description: string }>;
 }
 
 export const ProjectsData: Project[] = [
@@ -19,6 +21,34 @@ export const ProjectsData: Project[] = [
 		description: "Exclusive boxing workouts by Michael ‘Silk’ Olajide Jr.",
 		deployedLink: "https://systemofsilk.com/",
 		image: projectImage,
+		overview:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		features: [
+			{
+				id: 1,
+				name: "Responsive Design",
+				description:
+					"Adapts seamlessly to all screen sizes — mobile, tablet, and desktop.",
+			},
+			{
+				id: 2,
+				name: "Dynamic Routing",
+				description:
+					"Uses dynamic routes to generate pages based on content or user input (e.g. /projects/[slug]).",
+			},
+			{
+				id: 3,
+				name: "Dark Mode Support",
+				description:
+					"Automatically switches themes based on system settings or user toggle.",
+			},
+			{
+				id: 4,
+				name: "Search Functionality",
+				description:
+					"Allows users to filter or find content easily using keywords.",
+			},
+		],
 	},
 	{
 		id: 2,
@@ -27,6 +57,34 @@ export const ProjectsData: Project[] = [
 		description: "Track expiration date of makeup products",
 		githubLink: "https://github.com/ifechukwu123/beauty-bin",
 		image: projectImage,
+		overview:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		features: [
+			{
+				id: 1,
+				name: "Animated Interactions",
+				description:
+					"Smooth transitions and hover animations enhance user experience.",
+			},
+			{
+				id: 2,
+				name: "Accessibility Features",
+				description:
+					"Includes semantic HTML, keyboard navigation, and ARIA labels for inclusive design.",
+			},
+			{
+				id: 3,
+				name: "Form Validation",
+				description:
+					"Provides real-time feedback on user input with clear error messaging.",
+			},
+			{
+				id: 4,
+				name: "Modular Components",
+				description:
+					"Built with reusable and maintainable components for scalability.",
+			},
+		],
 	},
 	{
 		id: 3,
@@ -35,5 +93,32 @@ export const ProjectsData: Project[] = [
 		description: "Full-Stack inventory management system",
 		githubLink: "https://github.com/ifechukwu123/InStock",
 		image: projectImage,
+		overview:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		features: [
+			{
+				id: 1,
+				name: "Image Optimization",
+				description:
+					"Lazy loading and responsive images ensure fast performance.",
+			},
+			{
+				id: 2,
+				name: "404 Page Handling",
+				description: "Custom error pages to handle invalid URLs gracefully.",
+			},
+			{
+				id: 3,
+				name: "Form Validation",
+				description:
+					"Provides real-time feedback on user input with clear error messaging.",
+			},
+			{
+				id: 4,
+				name: "Modular Components",
+				description:
+					"Built with reusable and maintainable components for scalability.",
+			},
+		],
 	},
 ];
