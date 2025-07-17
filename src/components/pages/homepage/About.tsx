45d7f8156d024599f9d34ed6cf8f15b3848f9764
Ife Onuorah <ifeonuorah@gmail.com>
2025-07-17 15:02:22 -0300

    Update h1 sizes for all screen sizes as they are too big

--- src/components/pages/homepage/About.tsx
+++ src/components/pages/homepage/About.tsx
@@ -12,7 +12,7 @@ export default function About() {
 		<section className="px-sm md:px-[12%] py-[2rem] md:py-[3rem] lg:py-[4rem] h-[calc(100vh-4.4375rem)] md:h-[calc(100vh-4.6875rem)] lg:h-[calc(100vh-5.5rem)] min-h-[35.625rem] md:min-h-[30rem] flex flex-col-reverse md:flex-row justify-center md:justify-around items-center gap-4 md:gap-10 lg:gap-12">
 			<div className="flex-1 flex flex-col gap-6 md:gap-9 md:flex-1 md:max-w-[31.5625rem]">
 				<div className="flex flex-col items-center md:items-start md:gap-4">
-					<h1 className="capitalize flex items-baseline gap-1 md:gap-2">
+					<h1 className="capitalize flex items-baseline gap-1 md:gap-2 text-[3.5rem] leading-[5.25rem] md:text-[4.5rem] md:leading-[6.75rem] lg:text-[6rem] lg:leading-[9rem]">
 						<span>hello!</span>{" "}
 						<Image
 							src={SmileIcon}
