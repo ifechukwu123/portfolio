6b23c2c52112c289bc3203bf044551074a34f17a
Ife Onuorah <ifeonuorah@gmail.com>
2026-04-04 01:24:02 -0300

    Structure and style the skills section on the homepage responsively

--- src/components/Tool/Tool.tsx
+++ src/components/Tool/Tool.tsx
@@ -1,5 +1,16 @@
-// import styles from "./Tool.module.scss";
+import styles from "./Tool.module.scss";
 
-export default function Tool() {
-	return <>Tools</>;
+interface ToolProps {
+	name: string;
+	icon: string;
+}
+
+export default function Tool({ name, icon }: ToolProps) {
+	//Add tooltip for the skills
+	return (
+		<span className={styles.tool}>
+			<span className={styles.title}>{name}</span>
+			<img src={icon} alt="" aria-hidden="true" />
+		</span>
+	);
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import { useTheme } from "../../context/theme/ThemeProvider";
 import Button from "../../components/Button/Button";
 import SmileIcon from "../../assets/icons/smiling-emoji.svg";
 import profilePic from "../../assets/images/profile.png";
 import profilePicDark from "../../assets/images/profile-dark.png";
+import reactIcon from "../../assets/icons/tools/react.svg";
+import figmaIcon from "../../assets/icons/tools/figma.svg";
+import gitIcon from "../../assets/icons/tools/git.svg";
+import githubIcon from "../../assets/icons/tools/github.svg";
+import htmlIcon from "../../assets/icons/tools/html5.svg";
+import javascriptIcon from "../../assets/icons/tools/javascript.svg";
+import nextjsIcon from "../../assets/icons/tools/nextjs.svg";
+import nodejsIcon from "../../assets/icons/tools/nodejs.svg";
+import pythonIcon from "../../assets/icons/tools/python.svg";
+import tailwindIcon from "../../assets/icons/tools/tailwind.svg";
+import typescriptIcon from "../../assets/icons/tools/typescript.svg";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "../../context/theme/ThemeProvider";
 import styles from "./Home.module.scss";
+import Tool from "../../components/Tool/Tool";
+
+const skills = [
+	{ id: 1, name: "React", icon: reactIcon },
+	{ id: 2, name: "HTML5", icon: htmlIcon },
+	{ id: 3, name: "Javascript", icon: javascriptIcon },
+	{ id: 4, name: "Github", icon: githubIcon },
+	{ id: 5, name: "Typescript", icon: typescriptIcon },
+	{ id: 6, name: "Git", icon: gitIcon },
+	{ id: 7, name: "Next.js", icon: nextjsIcon },
+	{ id: 8, name: "Tailwind", icon: tailwindIcon },
+	{ id: 9, name: "Node.js", icon: nodejsIcon },
+	{ id: 10, name: "Python", icon: pythonIcon },
+	{ id: 11, name: "Figma", icon: figmaIcon },
+];
 
 export default function Home() {
 	const navigate = useNavigate();
@@ -41,7 +67,20 @@ export default function Home() {
 					className={styles.about__image}
 				/>
 			</section>
-			<section className={styles.skills}></section>
+			<section className={styles.skills}>
+				<h2 className={styles.skills__heading}>what i work with</h2>
+				<div className={styles["skills-container"]}>
+					<ul role="list" className={styles.skills__list}>
+						{/* Duplicate for marquee animation */}
+						{[...skills, ...skills].map((skill, index) => (
+							<li key={`${skill.id}-${index}`}>
+								<Tool name={skill.name} icon={skill.icon} />
+							</li>
+						))}
+						{/* What happens if the user doesn't like animation? Can they even scroll? */}
+					</ul>
+				</div>
+			</section>
 			<section className={styles.projects}></section>
 			<section className={styles.contact}></section>
 		</div>
